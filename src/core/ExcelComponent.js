411aa2c1712fa3fff9e00ad6c1bd0979dafd3f80
ValBomj <123itunes2002@gmail.com>
2020-09-06 01:39:06 +0300

    finish redux and table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,37 +1,57 @@
-import { DomListener } from '@core/DomListener'
+import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-	constructor($root, options = {}) {
-		super($root, options.listeners)
+  constructor($root, options = {}) {
+    super($root, options.listeners)
     this.name = options.name || ''
-		this.emitter = options.emitter
-		this.unsubscribers = []
+    this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
+    this.unsubscribers = []
 
+    this.prepare()
+  }
 
-		this.prepare()
-	}
+  // Настраивааем наш компонент до init
+  prepare() {}
 
-	prepare() {}
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
 
-	toHTML() {
-		return ''
-	}
+  // Уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
 
-	$emit(event, ...args) {
-		this.emitter.emit(event, ...args)
-	}
+  // Подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
 
-	$on(event, fn) {
-		const unsub = this.emitter.subscribe(event, fn)
-		this.unsubscribers.push(unsub)
-	}
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
 
-	init() {
-		this.initDOMListeners()
-	}
+  // Сюда приходят только изменения по тем полям, на которые мы подписались
+  storeChanged() {}
 
-	destroy() {
-		this.removeDOMListeners()
-		this.unsubscribers.forEach(unsub => unsub())
-	}
+  isWatching(key) {
+    return this.subscribe.includes(key)
+  }
+
+  // Инициализируем компонент
+  // Добавляем DOM слушателей
+  init() {
+    this.initDOMListeners()
+  }
+
+  // Удаляем компонент
+  // Чистим слушатели
+  destroy() {
+    this.removeDOMListeners()
+    this.unsubscribers.forEach(unsub => unsub())
+  }
 }
