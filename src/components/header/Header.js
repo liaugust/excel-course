70bf42c19800b9d14234ab0470ac2a071ba4fb33
ValBomj <123itunes2002@gmail.com>
2020-09-06 19:39:02 +0300

    Routing implemented

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,39 +2,54 @@ import { ExcelComponent } from '@core/ExcelComponent'
 import { $ } from '@core/dom'
 import { changeTitle } from '../../redux/actions'
 import { defaultTitle } from '../../constants'
+import { ActiveRoute } from '@core/routes/ActiveRoute'
 
 export class Header extends ExcelComponent {
-  static className = 'excel__header'
+	static className = 'excel__header'
 
-  constructor($root, options) {
-    super($root, {
-      name: 'Header',
-      listeners: ['input'],
-      ...options,
-    })
-  }
+	constructor($root, options) {
+		super($root, {
+			name: 'Header',
+			listeners: ['input', 'click'],
+			...options,
+		})
+	}
 
-  toHTML() {
-    const title = this.store.getState().title || defaultTitle
-    return `
+	toHTML() {
+		const title = this.store.getState().title || defaultTitle
+		return `
       <input type="text" class="input" value="${title}">
 
       <div>
 
-        <div class="button">
-          <i class="material-icons">delete</i>
+        <div class="button" data-button="remove">
+          <i class="material-icons" data-button="remove">delete</i>
         </div>
 
-        <div class="button">
-          <i class="material-icons">exit_to_app</i>
+        <div class="button" data-button="exit">
+          <i class="material-icons" data-button="exit">exit_to_app</i>
         </div>
 
       </div>
     `
-  }
+	}
 
-  onInput(event) {
-    const $target = $(event.target)
-    this.$dispatch(changeTitle($target.text()))
-  }
+	onClick(event) {
+		const $target = $(event.target)
+		if ($target.data.button === 'remove') {
+      const decision = confirm('Вы действительно хотите удалить эту таблицу?')
+
+      if (decision) {
+        localStorage.removeItem('excel:' + ActiveRoute.param)
+        ActiveRoute.navigate('')
+      }
+		} else if ($target.data.button === 'exit') {
+      ActiveRoute.navigate('')
+		}
+	}
+
+	onInput(event) {
+		const $target = $(event.target)
+		this.$dispatch(changeTitle($target.text()))
+	}
 }
